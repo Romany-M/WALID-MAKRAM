ac85bd405a21c9d6c11e1b96e09b1c3179bc2cf2
Romany Makram <romanymakram374@gmail.com>
2026-04-30 02:47:09 +0300

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const base = "https://walid-makram.vercel.app";
+  const base = "https://walidmakram.art";
   const now  = new Date();
 
   return [
